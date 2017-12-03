cbb474c78af0bf408244552f750426da44f3044b
russellpierce <russell.s.pierce@gmail.com>
2017-12-03 05:56:16 +0000

    Update tests

--- tests/testthat/testHelloWorld.R
+++ tests/testthat/testHelloWorld.R
@@ -1,7 +1,3 @@
-# A hack to make CI builds work on windows/appveyor
-# See https://github.com/krlmlr/r-appveyor/issues/91
-.libPaths(c("C:/RLibrary",.libPaths()))
-
 library(reticulate)
 library(rboto3)
 
